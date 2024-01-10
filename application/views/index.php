01c57de1cc18757da81222e99a5cca47aba7ac22
Yash Kumar | Team Ethinos <yash.kumar@ethinos.com>
2024-01-10 23:59:12 +0530

    User Login Registration Complete

--- application/views/index.php
+++ application/views/index.php
@@ -20,49 +20,50 @@
 	<link href="admin-assets/css/app.css" rel="stylesheet">
 	<link href="admin-assets/css/icons.css" rel="stylesheet">
 	<title>easyprofile - user login</title>
+	<style>
+		.form-control {opacity:50%;}
+		.btn-primary {background-color:#6d6d6e;border-color:#6d6d6e;}
+		.btn-primary:hover {background-color:black;border-color:black;}
+		a{color:#6d6d6e;}
+		a:hover{color:black;}
+	</style>
 </head>
 
 <body class="">
 	<div class="container">
 		<div class="row justify-content-center">
-			<div class="col-md-5">
-				<div class="aligns-items-center justify-content-center card text-center mt-5">
+			<div class="col-md-8 mt-5">
+				<div class="aligns-items-center justify-content-center card text-center mt-5" style="background-image: url('admin-assets/images/login-bg.png');">
 					<div class="card-body">
-						<div class="row justify-content-center">
-							<div class="col-md-6">
+						<div class="row ">
+							<div class="col-md-3 text-start mb-4">
 								<img src="logo/logo-black.png" class="w-100" alt="" />
 							</div>
 							<div class="col-md-12 text-start">
-								<div class="text-center mb-4">
+								<div class="mb-4">
 									<h5 class="mb-0">Please log in to your account</h5>
 								</div>
-								<form class="row g-3" action="user/dashboard/">
-									<div class="col-12">
-										<label for="inputEmailAddress" class="form-label">Email</label>
-										<input type="email" class="form-control" id="inputEmailAddress" placeholder="jhon@example.com">
+								<form class="g-3" action="user/dashboard/">
+									<div class="col-md-6 mb-3">
+										<label for="inputEmailAddress" class="form-label">Email<span class="text-danger">*<span></label>
+										<input type="email" class="form-control" id="inputEmailAddress" placeholder="Enter Email Id." require>
 									</div>
-									<div class="col-12">
-										<label for="inputChoosePassword" class="form-label">Password</label>
-										<div class="input-group" id="show_hide_password">
-											<input type="password" class="form-control border-end-0" id="inputChoosePassword" value="12345678" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+									<div class="col-md-6 mb-3">
+										<label for="inputChoosePassword" class="form-label">Password<span class="text-danger">*<span></label>
+										<div class="input-group" id="show_hide_password" >
+											<input type="password" class="form-control border-end-0" require id="inputChoosePassword"  placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
 										</div>
 									</div>
-									<div class="col-md-6">
-										<div class="form-check form-switch">
-											<input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
-											<label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
-										</div>
+									<div class="col-md-6 text-end mb-3">	<a href="authentication-forgot-password.html">Forgot Password ?</a>
 									</div>
-									<div class="col-md-6 text-end">	<a href="authentication-forgot-password.html">Forgot Password ?</a>
-									</div>
-									<div class="col-12">
+									<div class="col-md-6 mb-3">
 										<div class="d-grid">
-											<button type="submit" class="btn btn-primary">Sign in</button>
+											<button type="submit" class="btn btn-primary">Login</button>
 										</div>
 									</div>
-									<div class="col-12">
+									<div class="col-md-6 mb-3">
 										<div class="text-center ">
-											<p class="mb-0">Don't have an account yet? <a href="authentication-signup.html">Sign up here</a>
+											<p class="mb-0">Don't have an account yet? <a href="#" data-bs-toggle="modal" data-bs-target="#userRegistration">Sign up here</a>
 											</p>
 										</div>
 									</div>
@@ -75,73 +76,102 @@
 		</div>
 	</div>
 
-	<!--wrapper-->
-	<!-- <div class="wrapper">
-		<div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
-			<div class="container">
-				<div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-					<div class="col mx-auto">
-						<div class="card mb-0">
-							<div class="card-body">
-								<div class="p-4">
-									<div class="mb-3 text-center">
-										<img src="logo/logo-black.png" class="w-100" alt="" />
-									</div>
-									<div class="text-center mb-4">
-										<p class="mb-0">Please log in to your account</p>
-									</div>
-									<div class="form-body">
-										<form class="row g-3">
-											<div class="col-12">
-												<label for="inputEmailAddress" class="form-label">Email</label>
-												<input type="email" class="form-control" id="inputEmailAddress" placeholder="jhon@example.com">
-											</div>
-											<div class="col-12">
-												<label for="inputChoosePassword" class="form-label">Password</label>
-												<div class="input-group" id="show_hide_password">
-													<input type="password" class="form-control border-end-0" id="inputChoosePassword" value="12345678" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
-												</div>
-											</div>
-											<div class="col-md-6">
-												<div class="form-check form-switch">
-													<input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
-													<label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
-												</div>
-											</div>
-											<div class="col-md-6 text-end">	<a href="authentication-forgot-password.html">Forgot Password ?</a>
-											</div>
-											<div class="col-12">
-												<div class="d-grid">
-													<button type="submit" class="btn btn-primary">Sign in</button>
-												</div>
-											</div>
-											<div class="col-12">
-												<div class="text-center ">
-													<p class="mb-0">Don't have an account yet? <a href="authentication-signup.html">Sign up here</a>
-													</p>
-												</div>
-											</div>
-										</form>
-									</div>
-									<div class="login-separater text-center mb-5"> <span>OR SIGN IN WITH</span>
-										<hr/>
-									</div>
-									<div class="list-inline contacts-social text-center">
-										<a href="javascript:;" class="list-inline-item bg-facebook text-white border-0 rounded-3"><i class="bx bxl-facebook"></i></a>
-										<a href="javascript:;" class="list-inline-item bg-twitter text-white border-0 rounded-3"><i class="bx bxl-twitter"></i></a>
-										<a href="javascript:;" class="list-inline-item bg-google text-white border-0 rounded-3"><i class="bx bxl-google"></i></a>
-										<a href="javascript:;" class="list-inline-item bg-linkedin text-white border-0 rounded-3"><i class="bx bxl-linkedin"></i></a>
-									</div>
-
-								</div>
-							</div>
-						</div>
+<!-- ==========Registration Modal============== -->
+<div class="modal fade" id="userRegistration" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<div class="row">
+					<div class="col-md-6">
+						<img src="logo/logo-black.png" class="w-100" alt="" />
 					</div>
 				</div>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+				<div class="text-center mb-4">
+					<h5 class="mb-0">Please Register Your Self !</h5>
+				</div>
+				<form class="row g-3" action="">
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">First Name<span class="text-danger">*<span></label>
+						<input type="text" class="form-control" id="firstName" placeholder="Enter Your First Name" required>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">Last Name<span class="text-danger">*<span></label>
+						<input type="text" class="form-control" id="lastName" placeholder="Enter Your Last Name" require>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">Mobile No.<span class="text-danger">*<span></label>
+						<input type="number" class="form-control" id="mobileNo" placeholder="Enter Your Mobile No." require>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">Email<span class="text-danger">*<span></label>
+						<input type="email" class="form-control" id="emailId" placeholder="Enter Your Email Id." require>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">Country<span class="text-danger">*<span></label>
+						<select class="form-select" id="country" name="country" require>
+							<option selected="" disabled="" value="">Choose...</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">State<span class="text-danger">*<span></label>
+						<select class="form-select" id="state" name="state" require>
+							<option selected="" disabled="" value="">Choose...</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">City<span class="text-danger">*<span></label>
+						<select class="form-select" id="city" name="city" require>
+							<option selected="" disabled="" value="">Choose...</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+					</div>
+					<div class="col-md-6">
+						<label for="inputEmailAddress" class="form-label">PIN Code<span class="text-danger">*<span></label>
+						<input type="number" class="form-control" id="pinCode" placeholder="Enter PIN Code">
+					</div>
+					<div class="col-md-6">
+						<label for="inputChoosePassword" class="form-label">Password<span class="text-danger">*<span></label>
+						<div class="input-group" id="show_hide_password">
+							<input type="password" class="form-control border-end-0" id="password" value="12345678" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="inputChoosePassword" class="form-label">Confirm Password<span class="text-danger">*<span></label>
+						<div class="input-group" id="show_hide_password">
+							<input type="password" class="form-control border-end-0" id="confirmPassword" value="12345678" placeholder="Confirm Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+						</div>
+					</div>
+
+					<div class="col-12">
+						<div class="d-grid">
+							<button type="submit" class="btn btn-primary">Save</button>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="text-center ">
+							<p class="mb-0">Already have an account ? <a href="#" data-bs-dismiss="modal" aria-label="Close"> Login in here</a>
+							</p>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
-	</div> -->
-	<!--end wrapper-->
+	</div>
+</div>
+<!-- ========================================== -->
+
+
 	<!-- Bootstrap JS -->
 	<script src="admin-assets/js/bootstrap.bundle.min.js"></script>
 	<!--plugins-->
