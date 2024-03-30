e580ddafe10a837ac447b2724d3d84e567701eab
MahekBSPL <mahak.verma@bharuwasolutions.com>
2024-03-30 11:42:27 +0530

    all development

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,6 +56,7 @@ $route['translate_uri_dashes'] = FALSE;
 $route['UserDashboard'] = 'UserDashboard/dashboard';
 $route['logoutuser']='Login/LogoutUser';
 $route['save_user_img']='User/save_user_img';
+
 $route['View/(:any)'] = 'Home/index/$1';
 
 
