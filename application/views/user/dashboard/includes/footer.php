34308646653153adf83cdb3ecdb51b9e65433cec
MahekBSPL <mahak.verma@bharuwasolutions.com>
2024-04-13 22:53:53 +0530

    modify code

--- application/views/user/dashboard/includes/footer.php
+++ application/views/user/dashboard/includes/footer.php
@@ -173,6 +173,7 @@
 				}
 		}
 		function onlyAlphabets(charCode) {
+			var charCode = event.which || event.keyCode;
     if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32) {
         return true;
     } else {
